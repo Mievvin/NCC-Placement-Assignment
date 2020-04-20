4504b58c673cdce72a12f720b28be8fca08d72e0
Wojtek Piekarczyk <piekarczykw@gmail.com>
2020-04-20 19:49:54 +0100

    Connected web app to NHS API, displays content upon button press

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 
 namespace NHSWebApp
 {
@@ -22,5 +23,6 @@ namespace NHSWebApp
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
