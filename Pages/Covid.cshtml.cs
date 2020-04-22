2cd9257de21377fad58e000521c773c4461a88e5
Wojtek Piekarczyk <piekarczykw@gmail.com>
2020-04-22 18:56:59 +0100

    Implemented page regarding COVID-19, linked it up with the COVID-19 API to show symptoms and treatment, changed styles

--- Pages/Covid.cshtml.cs
+++ Pages/Covid.cshtml.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace NHSWebApp.Pages
 {
     public class CovidModel : PageModel
     {
+        private readonly ILogger<CovidModel> _logger;
+
+        public CovidModel(ILogger<CovidModel> logger)
+        {
+            _logger = logger;
+        }
         public void OnGet()
         {
 
