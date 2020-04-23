3cb1bc2cfea2cc7b7082e55b75ca8664b3fb83b2
Wojtek Piekarczyk <piekarczykw@gmail.com>
2020-04-23 21:12:11 +0100

    Added login functionality by applying an individual user accounts authentication and scaffolding elements to change, made user condition display on the site, removed old code for doing this

--- Pages/Prescription.cshtml.cs
+++ Pages/Prescription.cshtml.cs
@@ -25,34 +25,8 @@ namespace NHSWebApp.Pages
 
         public void OnGet()
         {
-            //UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            
             
         }
-        /*
-        // Stores UserManager
-        private readonly UserManager<ApplicationUser> _manager;
-
-        // Inject UserManager using dependency injection.
-        // Works only if you choose "Individual user accounts" during project creation.
-        public PrivacyModel(UserManager<ApplicationUser> manager)
-        {
-            _manager = manager;
-        }
-
-        // You can also just take part after return and use it in async methods.
-        private async Task<ApplicationUser> GetCurrentUser()
-        {
-            return await _manager.GetUserAsync(HttpContext.User);
-        }
-
-        // Generic demo method.
-        public async Task DemoMethod()
-        {
-            var user = await GetCurrentUser();
-
-            string userId = user.Id;
-            string userCondition = user.Condition;
-        }
-        */
     }
 }
