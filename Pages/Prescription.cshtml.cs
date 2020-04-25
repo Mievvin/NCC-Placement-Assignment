8b3ab5392c541cbb047e907a43ab30df67d29065
Wojtek Piekarczyk <piekarczykw@gmail.com>
2020-04-25 16:56:47 +0100

    Implemented functionality to send an email with treatment information to the account-bound email, created new page for when the email is sent successfully

--- Pages/Prescription.cshtml.cs
+++ Pages/Prescription.cshtml.cs
@@ -9,24 +9,121 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using NCCWebApp.Models;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using NCCWebApp.Data;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using RestSharp;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Nancy.Json;
+using Microsoft.AspNetCore.Razor.Language;
 
 namespace NHSWebApp.Pages
 {
     [Authorize]
     public class PrescriptionModel : PageModel
     {
-        public string UserId { get; set; }
-        private readonly ILogger<PrescriptionModel> _logger;
-
-        public PrescriptionModel(ILogger<PrescriptionModel> logger)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public PrescriptionModel(UserManager<ApplicationUser> userManager)
         {
-            _logger = logger;
+            _userManager = userManager;
         }
 
-        public void OnGet()
+        [BindProperty, EmailAddress, Required, Display(Name = "Your Email Address")]
+        public string OrderEmail { get; set; }
+
+        private async Task LoadAsync(ApplicationUser user)
         {
-            
-            
+            var userName = await _userManager.GetUserNameAsync(user);
+            OrderEmail = userName;
         }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        class RootObject
+        {
+            [JsonProperty("hasPart")]
+            public List<hasPart> hasPart { get; set; }
+        }
+
+        class hasPart
+        {
+            [JsonProperty("@type")]
+            public string type { get; set; }
+            [JsonProperty("name")]
+            public string name { get; set; }
+            [JsonProperty("url")]
+            public string url { get; set; }
+            [JsonProperty("description")]
+            public string description { get; set; }
+            [JsonProperty("text")]
+            public string text { get; set; }
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            if (ModelState.IsValid)
+            {
+                var pageURL = "https://api.nhs.uk/conditions/";
+                var finalPageURL = String.Concat(pageURL, user.Condition);
+                var subscriptionKey = "a76e7e0f594f4457bbcfaf981eb295be";
+
+                var client = new RestClient(finalPageURL);
+                var request = new RestRequest();
+                request.Method = Method.GET;
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("subscription-key", subscriptionKey);
+                IRestResponse response = client.Execute(request);
+                var content = response.Content;
+
+                RootObject obj = new JavaScriptSerializer().Deserialize<RootObject>(content);
+                var symptomstext = "";
+                foreach (var item in obj.hasPart)
+                {
+                    if (item.name== "self_care")
+                    {
+                        symptomstext = item.text;
+                    }
+                }
+
+                var body = $@"Hello {user.Email} ! Please see below to see the prescription regarding {user.Condition} .<br/>
+                {symptomstext}";
+                using (var smtp = new SmtpClient())
+                {
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtp.PickupDirectoryLocation = @"C:\MailPickup";
+                    var message = new MailMessage();
+                    message.To.Add(OrderEmail);
+                    message.Subject = $@"NHSWebApp - Your {user.Condition} prescription";
+                    message.Body = body;
+                    message.IsBodyHtml = true;
+                    message.From = new MailAddress("treatment@nhswebapp.co.uk");
+                    await smtp.SendMailAsync(message);
+                }
+                return RedirectToPage("EmailSuccess");
+            }
+            return Page();
+        }
+
     }
 }
+
